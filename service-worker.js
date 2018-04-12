7b81c8d964eec01b268a784e214c84e30522daf8
slawomirtalarczyk <slawomir.talarczyk@yahoo.com>
2018-04-12 15:16:35 +0200

    Revert install scripts

--- service-worker.js
+++ service-worker.js
@@ -34,15 +34,15 @@ var filesToCache = [
   // '/images/wind.png'
 ];
 
-// self.addEventListener('install', function(e) {
-//   console.log('[ServiceWorker] Install');
-//   e.waitUntil(
-//     caches.open(cacheName).then(function(cache) {
-//       console.log('[ServiceWorker] Caching app shell');
-//       return cache.addAll(filesToCache);
-//     })
-//   );
-// });
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
